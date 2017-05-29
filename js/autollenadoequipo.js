5cff38ff5d20a21ec339b2efea785db578d62f33
luisfecastro94a <luisfecastro94@gmail.com>
2017-05-29 19:40:38 -0400

    update 29/05

--- js/autollenadoequipo.js
+++ js/autollenadoequipo.js
@@ -4,6 +4,12 @@ function listaequipo(id){
 		$('#sintoma_cliente').val(da[1]);
 		$('#marca').val(da[2]);
 		$('#estado').val(da[3]);
+		$('#nombre').val(da[4]);
+		$('#rut').val(da[5]);
+		$('#fono').val(da[6]);
+		$('#correo').val(da[7]);
+		$('#direccion').val(da[8]);
+		$('#nombrec').val(da[9]);
 	
 
 	});

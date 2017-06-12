78feb263f7d04a6d432cae3ccf66b79192c6bece
luisfecastro94a <luisfecastro94@gmail.com>
2017-06-12 09:19:41 -0400

    update 12/06

--- js/autollenadoequipo.js
+++ js/autollenadoequipo.js
@@ -1,17 +1,18 @@
 function listaequipo(id){
 	$.post("equipoautollenado.php",{id_equipo:id}, function(retorna){
 		da = retorna.split("/");
-		$('#sintoma_cliente').val(da[1]);
-		$('#marca').val(da[2]);
-		$('#estado').val(da[3]);
-		$('#nombre').val(da[4]);
-		$('#rut').val(da[5]);
-		$('#fono').val(da[6]);
-		$('#correo').val(da[7]);
-		$('#direccion').val(da[8]);
-		$('#nombreE').val(da[9]);
+		$('#serie_equipo').val(da[1]);
+		$('#sintoma_cliente').val(da[2]);
+		$('#marca').val(da[3]);
+		$('#estado').val(da[4]);
+		$('#nombre').val(da[5]);
+		$('#nombre').val(da[6]);
+		$('#fono').val(da[7]);
+		$('#correo').val(da[8]);
+		$('#direccion').val(da[9]);
+		$('#id_cliente').val(da[10]);
+		$('#nombreE').val(da[11]);
 		
-	
 	});
 
 }
